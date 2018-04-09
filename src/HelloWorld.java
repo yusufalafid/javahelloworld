c2c2be3ca93fe210511c2fa02b01e9b7571c4eac
aryulianto <saputroyulianto@gmail.com>
2018-04-09 14:14:22 +0700

    nambah #2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 	public static void main (String [] args) {
 		System.out.println("Hello World again");
-		System.out.println("Latihan auto build image");
+		System.out.println("Latihan auto build image #2");
 	}
 }
